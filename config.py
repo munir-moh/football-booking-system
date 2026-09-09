a51bd2f836ab0d1bbd50736d97ccb4d3ed46f22e
munir-moh <munirmuhd120@gmail.com>
2026-09-09 02:35:08 +0100

    Add dotenv support for environment variable management

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 ADMIN_PASSWORD = "supersecret123"
 PRICE_PER_HOUR = 10000
 MIN_HOURS = 1
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
